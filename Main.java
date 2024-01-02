a383c2e5dbfea9be1faf471df5520eac1a5ee4d5
COWBOY2323 <92857542+COWBOY2323@users.noreply.github.com>
2024-01-02 09:50:55 +0000

    Done adding output

--- Main.java
+++ Main.java
@@ -1,1 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Step 2: Create an int variable and name it check_number with an initial value of 10.
+        int check_number = 10;
+
+        // Step 3: Create a String variable and name it message.
+        String message;
+
+        // Step 4: Use check_number as a condition on how many loops the program can print.
+        for (int i = 1; i <= check_number; i++) {
+            // Step 5: Create a loop that will check and print each number if odd or even.
+            message = (i % 2 == 0) ? i + " is even number" : i + " is odd number";
+
+            // Step 6: Print the message value.
+            System.out.println(message);
+        }
+    }
+}
 
